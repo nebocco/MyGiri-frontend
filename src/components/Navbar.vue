ad931c31be77d42876fe08f12df6911e2cf2fa4c
nebocco <nebocco@gmail.com>
2021-07-10 16:22:06 +0900

    Title font

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,29 @@
 <template>
-  <header class="navbar-container" :class="isOpen ? 'opened' : 'closed'">
-    <div class="mini-container">
-      <h1 class><router-link to="/">TITLE</router-link></h1>
-      <img
-        id="mobile-cta"
-        src="@/assets/menu.svg"
-        alt="Open Navigation"
-        @click="toggleNavbar"
-      >
-    </div>
-    <nav :class="isOpen ? 'opened' : 'closed'">
-      <ul>
-        <li><router-link to="/" @click="isOpen=false;">ホーム</router-link></li>
-        <li v-if="!isLoggedIn"><router-link to="/register" @click="isOpen=false;">新規登録</router-link></li>
-        <li v-if="!isLoggedIn"><router-link to="/login" @click="isOpen=false;">ログイン</router-link></li>
-        <li v-if="isLoggedIn"><router-link to="/mypage" @click="isOpen=false;">マイページ</router-link></li>
-        <li v-if="isLoggedIn"><router-link to="/" @click="$refs.confirm.toggle()">ログアウト</router-link></li>
-      </ul>
-    </nav>
+  <div class="container">
+    <header class="navbar-container" :class="isOpen ? 'opened' : 'closed'">
+      <div class="mini-container">
+        <h1 class="title"><router-link to="/">まい喜利</router-link></h1>
+        <img
+          id="mobile-cta"
+          src="@/assets/menu.svg"
+          alt="Open Navigation"
+          @click="toggleNavbar"
+        >
+      </div>
+      <nav :class="isOpen ? 'opened' : 'closed'">
+        <ul>
+          <li><router-link to="/" @click="isOpen=false;">ホーム</router-link></li>
+          <li v-if="!isLoggedIn"><router-link to="/register" @click="isOpen=false;">新規登録</router-link></li>
+          <li v-if="!isLoggedIn"><router-link to="/login" @click="isOpen=false;">ログイン</router-link></li>
+          <li v-if="isLoggedIn"><router-link to="/mypage" @click="isOpen=false;">マイページ</router-link></li>
+          <li v-if="isLoggedIn"><router-link to="/" @click="$refs.confirm.toggle()">ログアウト</router-link></li>
+        </ul>
+      </nav>
+    </header>
     <ConfirmModal ref="confirm" @ok="{isOpen=false; logout();}">
       <p>ログアウトしますか？</p>
     </ConfirmModal>
-  </header>
+  </div>
 </template>
 
 <script lang="ts">
@@ -49,7 +51,7 @@ export default defineComponent({
         data: {}
       })
       .then((response) => {
-        console.log(response);
+        // console.log(response);
         store.dispatch('resetData');
         router.push('/');
       });
@@ -81,7 +83,8 @@ header {
     display: flex;
     justify-content: space-between;
     
-    h1 {
+    h1.title {
+      font-family: 'Stick', 'Chihaya Kaku';
       display: inline-block;
       font-size: 1.6rem;
       text-align: start;
